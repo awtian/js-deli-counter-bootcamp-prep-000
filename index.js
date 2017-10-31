876a6952d4ebd3f283e4c245fe95e6f7f4f32d39
Awtian Akbar <me@awtian.com>
2017-10-31 04:09:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function takeANumber(array, name) {
 function nowServing(x) {
   if (x.length > 0) {
   return `Currently serving ${x[0]}.`
-  x.shift
+  x.shift()
 }
 else {
   return `There is nobody waiting to be served!`
