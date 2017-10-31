c02c4d4da31ac360f6539eeda1ba7d19414018bd
Awtian Akbar <me@awtian.com>
2017-10-31 04:04:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ function takeANumber(array, name) {
   return `Welcome, ${name}. You are number ${array.length} in line.`
 };
 
-function newServing(x) {
-  return x[0]
+function nowServing(x) {
+  return `Currently serving ${x[0]}.`
 };
 function currentLine() {
 
