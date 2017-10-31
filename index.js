4810df434e90fcd4cb7a9b552f4bd7c66faade22
Awtian Akbar <me@awtian.com>
2017-10-31 03:57:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var katzDeli = [];
 function takeANumber(array, name) {
   return name
   array.push(name)
-  return name
-  return `Welcome, ${katzDeli[0]}. You are number ${katzDeli.length} in line.`
+  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
 };
 
 function newServing() {
