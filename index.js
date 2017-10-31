be6aa77c7ceee186d6ca34aacc62e509e06c198c
Awtian Akbar <me@awtian.com>
2017-10-31 03:42:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var katzDeli = {};
+var katzDeli = [];
 function takeANumber(name) {
+  var katzDeliLine = [];
   katzDeli.push(name)
   return `You are number ${katzDeli.length} in line`
 };
+
 function newServing() {
 
 };
