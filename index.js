106091c1602a51b53794dc85ba15adcbe2f58608
Awtian Akbar <me@awtian.com>
2017-10-31 05:04:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ else {
 };
 function currentLine(x) {
   if (x.length > 0) {
-    for (var i= 0 ; i == x.length; i++) {
+    for (var i= 0 ; i < x.length; i++) {
       return `The line is currently: ${parseInt(i) + 1}. ${x[i]}`
     }
   }
